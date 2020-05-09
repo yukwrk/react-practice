e19a54ff025d0c66734d476a6bf26003996e9dfc
yukwrk <yukwrk@gmail.com>
2020-05-09 15:59:36 +0900

    learn component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("Im clicked.")}} />
-      </React.Fragment>
-    ) 
-  }
+// class App extends Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("Im clicked.")}} />
+//       </React.Fragment>
+//     ) 
+//   }
+// }
+
+const Cat = () => {
+  return <div>myao!</div>;
+}
+
+const App = () => {
+  return(
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
 export default App;
